a0acfe70a89e3abde668b852f769fee69da4032a
Toshiaki Seino <st12318@gmail.com>
2026-05-06 19:00:57 +0900

    Refine: consolidate template file validation in `Templates.render` and remove `read_template`

--- lib/nob/templates.rb
+++ lib/nob/templates.rb
@@ -6,19 +6,21 @@ module Nob
     Literal = Struct.new(:text)
     Variable = Struct.new(:operator)
 
+    # テンプレートをレンダリングした結果の文字列を返却する Facede メソッド
+    # @param title [String] ファイルのタイトル
+    # @param now [Time] 現在時刻
+    # @param path [Pathname, String] テンプレートファイルのパス, text と同時に指定すると ArgumentError
+    # @param text [String] テンプレートの文字列, path と同時に指定すると ArgumentError
+    # @return [String] レンダリング結果の文字列
+    # (String, Time, Pathname|String, String) -> String
     def self.render(title:, now:, path: nil, text: nil)
-      text ||= read_template(path)
+      raise ArgumentError, "path or text must be provided" unless path || text
+      raise ArgumentError, "path must exist" unless path && File.exist?(path)
+
+      text ||= File.read(path)
       return "" if text.nil?
+
       Renderer.render(text, title: title, now: now)
     end
-
-    def self.read_template(path)
-      return nil if path.nil?
-      unless File.exist?(path)
-        raise Nob::Error, "template file not found: #{path}"
-      end
-      File.read(path)
-    end
-    private_class_method :read_template
   end
 end
